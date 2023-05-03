fe22444a6e7262debe8d3c0e8982740ccd7574bb
Kevin Washington <kevin.washingtonbr@gmail.com>
2023-05-03 18:11:10 -0300

    feat: enable the wasd keys

--- keyboard.py
+++ keyboard.py
@@ -5,25 +5,25 @@ from phase import Phase
 class Keyboard:
     def keyboard_callback(keys, run, phase):
         # if left arrow key is pressed
-        if keys[pygame.K_LEFT] and phase.player.x > 0:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a] and phase.player.x > 0:
 
             # decrement in x co-ordinate
             phase.player.update_pos(-phase.d_x, 0)
 
         # if left arrow key is pressed
-        if keys[pygame.K_RIGHT] and phase.player.x < 500-phase.player.width:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d] and phase.player.x < 500-phase.player.width:
 
             # increment in x co-ordinate
             phase.player.update_pos(phase.d_x, 0)
 
         # if left arrow key is pressed
-        if keys[pygame.K_UP] and phase.player.y > 0:
+        if keys[pygame.K_UP] or keys[pygame.K_w] and phase.player.y > 0:
 
             # decrement in y co-ordinate
             phase.player.update_pos(0, -phase.d_y)
 
         # if left arrow key is pressed
-        if keys[pygame.K_DOWN] and phase.player.y < 500-phase.player.height:
+        if keys[pygame.K_DOWN] or keys[pygame.K_s] and phase.player.y < 500-phase.player.height:
             # increment in y co-ordinate
             phase.player.update_pos(0, phase.d_y)
 
